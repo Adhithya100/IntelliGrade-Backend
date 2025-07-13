01e059e504b5c3544e9ead5407ae5dac44774b5e
Dinesh <dinesh23110077@snuchennai.edu.in>
2025-07-13 13:46:48 +0530

    modified models for uploading data

--- models.py
+++ models.py
@@ -1,5 +1,18 @@
 from pydantic import BaseModel, EmailStr
+from streamlit import status
 
 class User(BaseModel):
     mail_id: EmailStr
     password: str
+    
+class Exam(BaseModel):
+    exam_name: str
+    subject: str
+    max_marks: int
+    exam_date: str
+    
+class AnswerKey(BaseModel):
+    question_no: int
+    question: str
+    ideal_ans: str
+    max_mark_per_q: int
